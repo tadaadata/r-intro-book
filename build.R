08772a23e7fcae2199b9c919172eb4edef3f9b00
jemus42 <jemus42@quantenbrot.de>
2017-08-06 07:37:33 +0200

    Add image dir, expand installation instructions

--- build.R
+++ build.R
@@ -1,9 +1,22 @@
 #! /usr/bin/env Rscript
 
+message("Cleaning up potential debug files…")
+
 if (file.exists("r-intro.Rmd")) {
   file.remove("r-intro.Rmd")
 }
 
+print("Copying assets…")
+# Copy image dir to output dir so paths don't mess up
+status <- file.copy("images", "book/", overwrite = T, recursive = T)
+
+if (all(status)) {
+  print("Successfully copied assets")
+} else {
+  warning("Something didn't work right!")
+}
+
+print("Rendering book…")
 # Build Website
 bookdown::render_book(input = ".", output_dir = "book", output_format = "bookdown::gitbook")
 
@@ -13,7 +26,12 @@ bookdown::render_book(input = ".", output_dir = "book", output_format = "bookdow
 # Build EPUB
 # bookdown::render_book(input = ".", output_dir = "book", output_format = "bookdown::epub_book")
 
+print("Checking who you are…")
+
 current_user <- Sys.info()[["user"]]
+
+print(paste0("Presumably you're ", current_user,
+             ", I guess. If not, please check things."))
 
 if (current_user == "Lukas") {
   book_dir  <- "~/Sync/public.tadaa-data.de/r-intro/book"
@@ -23,11 +41,20 @@ if (current_user == "Lukas") {
   book_dir <-  NA
 }
 
+print("Copying stuff…")
+
 if (is.na(book_dir)) {
-  message("No output directory defined, leaving everything as is.")
+  warning("No output directory defined, leaving everything as is.")
 } else {
   # Copy book
   files <- list.files("book", full.names = T)
-  file.copy(files, book_dir, overwrite = TRUE, recursive = TRUE)
+  status <- file.copy(files, book_dir, overwrite = TRUE, recursive = TRUE)
+  if (all(status)) {
+    print("Successfully copied stuff!")
+  } else {
+    warning("Something didn't work right!")
+  }
 }
 
+print("All done!")
+print(Sys.time())
