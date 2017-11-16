d9696da8f34fbd6b3b98f6bc694f9b0097cc570c
jemus42 <jemus42@quantenbrot.de>
2017-11-16 02:41:21 +0100

    formatting

--- build.R
+++ build.R
@@ -3,7 +3,7 @@
 t_start <- Sys.time()
 
 # Check dependencies ----
-cat("\n\nChecking if required stuff is installed…\n")
+cat("\n\nChecking if required stuff is installed...\n")
 
 pkgs <- c("bookdown", "svglite", "tadaatoolbox", "sjPlot", "sjmisc", "devtools",
           "haven", "readr", "dplyr", "ggplot2", "scales", "RColorBrewer", "viridis",
@@ -32,19 +32,19 @@ out_pdf   <- paste0(out_dir, "/", bookdown_yml$book_filename, ".pdf")
 out_epub  <- paste0(out_dir, "/", bookdown_yml$book_filename, ".epub")
 
 if (!file.exists(out_dir)) {
-  cat("Output directory", out_dir, "doesn't exist, creating that for you…\n")
+  cat("Output directory", out_dir, "doesn't exist, creating that for you...\n")
   dir.create(out_dir)
 }
 
 # Clean up artifacts that cause problems ----
-cat("Cleaning up potential debug files…\n")
+cat("Cleaning up potential debug files...\n")
 
 if (file.exists(debug_out)) {
   cat("Removing", debug_out, "\n")
   file.remove(debug_out)
 }
 if (file.exists("_bookdown_files")) {
-  cat("Removing \"_bookdown_files\"…\n")
+  cat("Removing \"_bookdown_files\"...\n")
   system(command = "rm -r _bookdown_files")
 }
 
@@ -53,7 +53,7 @@ if (length(htmls) != 0) {
   status <- file.remove(htmls)
 
   if (all(status)) {
-    cat("Removed html files at root directory…\n")
+    cat("Removed html files at root directory...\n")
   }
 }
 rm(htmls)
@@ -74,10 +74,10 @@ if (all(status)) {
 }
 
 # Render things ----
-cat("\nRendering things\n")
+cat("\nRendering things...\n\n")
 
 # Build Website
-cat("Rendering website…\n")
+cat("Rendering website...\n")
 bookdown::render_book(input = ".",
                       output_format = "bookdown::gitbook",
                       clean_envir = FALSE, quiet = TRUE)
@@ -90,17 +90,17 @@ if (file.exists(out_epub)) {
 }
 
 if (is.null(age) || age > 12) {
-  cat("Rendering epub…\n")
+  cat("Rendering epub...\n")
   bookdown::render_book(input = ".",
                         output_format = "bookdown::epub_book",
                         clean_envir = F, quiet = TRUE)
-  cat("Converting epub to PDF…\n")
+  cat("Converting epub to PDF...\n")
   bookdown::calibre(input = out_epub, output = out_pdf)
 }
 cat("Done rendering\n")
 
 # Prepare to copy to output ----
-cat("\nChecking who you are…\n")
+cat("\nChecking who you are...\n")
 current_user <- Sys.info()[["user"]]
 cat(paste0("I hope you're really ", current_user), "\n")
 
@@ -113,7 +113,7 @@ if (current_user == "Lukas") {
 }
 
 # Copy to output ----
-cat("\nCopying stuff…\n")
+cat("\nCopying stuff...\n")
 
 if (is.na(book_dir)) {
   warning("No output directory defined, leaving everything as is.")
@@ -130,7 +130,7 @@ if (is.na(book_dir)) {
 
 # Final Cleanup ----
 if (file.exists("_bookdown_files")) {
-  cat("Removing \"_bookdown_files\"…\n")
+  cat("Removing \"_bookdown_files\"...\n")
   system(command = "rm -r _bookdown_files")
 }
 
